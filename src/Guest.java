50b39941253ed686e1bcb86bfd19063f02fe9431
TrDucChinh <chinhdevptit@gmail.com>
2023-11-09 18:25:19 +0700

    Fix printf

--- src/Guest.java
+++ src/Guest.java
@@ -48,7 +48,7 @@ public class Guest extends LibrarySystem {
             System.out.print("Nhập truy vấn: ");
             String query = sc.nextLine();
             System.out.println("Kết quả tìm kiếm: ");
-            System.out.printf("%-30s%-30s%-30s%-30s\n", "Tên Sách", "Tác Giả", "Thể Loại", "Ngày Xuất Bản");
+            System.out.printf("%-10s%-30s%-30s%-30s%-30s\n", "STT", "Tên Sách", "Tác Giả", "Thể Loại", "Ngày Xuất Bản");
             int count = 1;
             boolean check = false;
             for (Book book : bookList) {
@@ -56,7 +56,7 @@ public class Guest extends LibrarySystem {
                         book.getAuthor().toLowerCase().contains(query.toLowerCase()) ||
                         book.getGenre().toLowerCase().contains(query.toLowerCase()) ||
                         book.getPublishDate().contains(query)) {
-                    System.out.printf("%-30s%-30s%-30s%-30s\n", book.getTitle(), book.getAuthor(), book.getGenre(), book.getPublishDate());
+                    System.out.printf("%-10d%-30s%-30s%-30s%-30s\n", count, book.getTitle(), book.getAuthor(), book.getGenre(), book.getPublishDate());
                     check = true;
                     ++count;
                 }
